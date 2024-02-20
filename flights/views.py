8abef210ae5545d0e3967d72f1c835b93fcf4509
Scarlett <sabinadinasarca.com>
2024-02-20 16:17:28 +0200

    added book feature

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Flight, Passenger
 
-from .models import Flight
+
 
 # Create your views here.
 def index(request):
@@ -11,5 +14,13 @@ def index(request):
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
     return render(request, "flights/flight.html", {
-        "flight": flight
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
+
+def book(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+    passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
